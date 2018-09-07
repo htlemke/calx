6446081b6ccf8d802117928ecb2e8d4f28c4a993
Henrik Lemke <henrik.lemke@psi.ch>
2018-09-07 15:00:21 +0200

    gaussian_beam

--- calx/pumpprobe.py
+++ calx/pumpprobe.py
@@ -10,3 +10,15 @@ def fluence(energy,area,inc_angle=np.pi/2,reflectivity=0):
     return energy*1e3/area/1e4*(1-reflectivity)*np.sin(inc_angle)
 
 
+def grazingIncTimeres(probeSize,probeAng, pumpAng):
+  """Calculates the time resulution in a 
+  grazing incidence pump probe geometry.
+  This respects only purely geometric factors of the 
+  surface. 
+  angles in radians
+  lengthscale in meters
+  --> result in seconds"""
+  footprint = probeSize/np.sin(probeAng)
+  lenprobe = probeSize/np.tan(probeAng)
+  lenpump = np.cos(pumpAng)*footprint
+  return (lenpump-lenprobe)/constants.c
