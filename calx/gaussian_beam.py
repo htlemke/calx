a20034be7dbf9244badceb345f1b8d265ce2a1a1
Henrik Lemke <htlemke@gmail.com>
2025-09-15 09:22:37 +0200

    fixes after long time

--- calx/gaussian_beam.py
+++ calx/gaussian_beam.py
@@ -1,29 +1,36 @@
 """Functions for simple calculations with gaussian beams."""
+
 import numpy as np
 
-def waist(lam,divergence,n=1):
-    """wavelength <lam> in same units as output, 
+
+def waist(lam, divergence, n=1):
+    """wavelength <lam> in same units as output,
     divergence (full divergence far from focus) in radians.
     <n> is refractive index."""
-    return lam/np.pi/divergence*2 /n
+    return lam / np.pi / divergence * 2 / n
 
-def wz(waist,z,lam,Msq=1.):
+
+def wz(z, lam, waist=None, divergence=None, n=1, Msq=1.0):
     """beam size at distance <z> from the waist in beam direction.
     <waist> (1/e radius in amplitude), <z> and <lam> should be same units,
     result unit will be the same."""
-    M = sqrt(Msq)
-    wz = w0*sqrt(M**2+M**2*(lam*z/np.pi/w0**2)**2)
+    if waist is None:
+        if divergence is None:
+            raise ValueError("Either waist or divergence must be given.")
+        waist = waist(lam, divergence, n=n)
+    M = np.sqrt(Msq)
+    wz = waist * np.sqrt(M**2 + M**2 * (lam * z / np.pi / waist**2) ** 2)
     return wz
 
-def rayleighRange(lam,waist):
+
+def rayleigh_range(lam, waist):
     """Rayleigh range at wavelength <lam> and <waist>.
     All inputs and outputs should have same unit."""
-    return np.pi*waist**2/lam
+    return np.pi * waist**2 / lam
 
-def gouyPhase(z,rayleighRange):
+
+def gouy_phase(z, rayleighRange):
     """calculates the gouy phase shift close to the focus.
-    distance from focus <z> and <rayleighRange> in same units, 
+    distance from focus <z> and <rayleighRange> in same units,
     result in radians"""
-    return np.arctan(z/rayleighRange)
-
-
+    return np.arctan(z / rayleighRange)
--- calx/materials.py
+++ calx/materials.py
@@ -1,18 +1,22 @@
 import xrayutilities as xu
 import xraylib_np
 from . import consts as _consts
-from scipy.constants import torr,bar,k,N_A,R
+from scipy.constants import torr, bar, k, N_A, R
 import numpy as np
 
 # This module holds relevant materials of the
 # xrayutilities materials class,
 
+
 class MaterialCollection:
-    """ Dummy class collections of materials (dict-like)."""
+    """Dummy class collections of materials (dict-like)."""
+
     def __init__(self, **entries):
         self.__dict__.update(entries)
-    def __setitem__(self,key,value):
-        self.__dict__.update({key:value})
+
+    def __setitem__(self, key, value):
+        self.__dict__.update({key: value})
+
 
 _amorphous = dict()
 _crystal = dict()
@@ -22,70 +26,104 @@ amorphous = MaterialCollection()
 crystal = MaterialCollection()
 gas = MaterialCollection()
 
-def _get_transmission(self,d,E='config'):
-    """ calculate the transmittion after thickness d (in m) of material at energy E (in eV)."""
-    return np.exp(-d*1e6/self.absorption_length(E))
+
+def _get_transmission(self, d, E="config"):
+    """calculate the transmittion after thickness d (in m) of material at energy E (in eV)."""
+    return np.exp(-d * 1e6 / self.absorption_length(E))
+
 
 xu.materials.Material.transmission = _get_transmission
 
-def _get_CS_Photo(self,E=None):
-  """Returns the total photoabsorption cross section in m^2
-     ID is the element symbol  E is the photon energy in eV
-     NOTE: This is per molecule if chemical formula given
-  """
-  E = np.atleast_1d(E)
-  CS=0
-  if hasattr(self,'base') and self.base:
-      base = self.base
-  elif hasattr(self,'lattice'):
-      base = []
-      for atom,position,amount,dontknow in self.lattice.base():
-          base.append((atom,amount))
-  else:
-      raise Exception('Did not find a base object!')
 
-  for atom,amount in base:
-    CS+=xraylib_np.CS_Photo(np.atleast_1d(atom.num),E)*amount*atom.weight
-  return CS
+def _get_CS_Photo(self, E=None):
+    """Returns the total photoabsorption cross section in m^2
+    ID is the element symbol  E is the photon energy in eV
+    NOTE: This is per molecule if chemical formula given
+    """
+    E = np.atleast_1d(E)
+    CS = 0
+    if hasattr(self, "base") and self.base:
+        base = self.base
+    elif hasattr(self, "lattice"):
+        base = []
+        for atom, position, amount, dontknow in self.lattice.base():
+            base.append((atom, amount))
+    else:
+        raise Exception("Did not find a base object!")
+
+    for atom, amount in base:
+        CS += xraylib_np.CS_Photo(np.atleast_1d(atom.num), E) * amount * atom.weight
+    return CS
+
 
 xu.materials.Material.cs_photo = _get_CS_Photo
 
 
-crystal['Si'] = xu.materials.Si
-crystal['Ge'] = xu.materials.Ge
-crystal['GaAs'] = xu.materials.GaAs
-crystal['Al'] = xu.materials.Al
-crystal['Diamond'] = xu.materials.C
-crystal['Be'] = xu.materials.material.Crystal("Be", \
-        xu.materials.spacegrouplattice.SGLattice(\
-        194, 2.2858, 3.5843, atoms=[xu.materials.elements.Be, ], \
-        pos=['2c', ]))
+crystal["Si"] = xu.materials.Si
+crystal["Ge"] = xu.materials.Ge
+crystal["GaAs"] = xu.materials.GaAs
+crystal["Al"] = xu.materials.Al
+crystal["Diamond"] = xu.materials.C
+crystal["Be"] = xu.materials.material.Crystal(
+    "Be",
+    xu.materials.spacegrouplattice.SGLattice(
+        194,
+        2.2858,
+        3.5843,
+        atoms=[
+            xu.materials.elements.Be,
+        ],
+        pos=[
+            "2c",
+        ],
+    ),
+)
 # crystal['Al2O3'] = xu.materials.material.Crystal("Al2O3", \
 #     xu.materials.spacegrouplattice.SGLattice(167, 0.4759026, 1.299084, atoms=[xu.materials.elements.Al, xu.materials.elements.O],pos=['18e','12c'])
 
-amorphous['B4C'] = xu.materials.material.Amorphous('B4C',2520,[('B',4),('C',1)])
-amorphous['Mo'] = xu.materials.material.Amorphous('Mo',10220,[('Mo',1)])
-amorphous['polyimide'] = xu.materials.material.Amorphous('polyimide',1430,[('C',22),('H',10),('N',2),('O',5)])
-amorphous['mylar'] = xu.materials.material.Amorphous('mylar',1400,[('C',10),('H',8),('O',4)])
-amorphous['polycarbonate'] = xu.materials.material.Amorphous('polycarbonate',1200,[('C',16),('H',14),('O',3)])
-amorphous['Si3N4'] = xu.materials.material.Amorphous('Silicon nitride',3440,[('Si',3),('N',4)])
+amorphous["B4C"] = xu.materials.material.Amorphous("B4C", 2520, [("B", 4), ("C", 1)])
+amorphous["Mo"] = xu.materials.material.Amorphous("Mo", 10220, [("Mo", 1)])
+amorphous["polyimide"] = xu.materials.material.Amorphous(
+    "polyimide", 1430, [("C", 22), ("H", 10), ("N", 2), ("O", 5)]
+)
+amorphous["mylar"] = xu.materials.material.Amorphous(
+    "mylar", 1400, [("C", 10), ("H", 8), ("O", 4)]
+)
+amorphous["polycarbonate"] = xu.materials.material.Amorphous(
+    "polycarbonate", 1200, [("C", 16), ("H", 14), ("O", 3)]
+)
+amorphous["Si3N4"] = xu.materials.material.Amorphous(
+    "Silicon nitride", 3440, [("Si", 3), ("N", 4)]
+)
 # amorphous['air'] = xu.materials.material.Amorphous('air',1000,[('N',1.562),('O',.42),('C',.0003),('Ar',.0094)])
-amorphous['SiC'] = xu.materials.material.Amorphous('Silicon carbide',3210,[('Si',1),('C',1)])
-
+amorphous["SiC"] = xu.materials.material.Amorphous(
+    "Silicon carbide", 3210, [("Si", 1), ("C", 1)]
+)
+amorphous["YAG"] = xu.materials.material.Amorphous(
+    "YAG", 4560, [("Y", 3), ("Al", 5), ("O", 12)]
+)
+amorphous["steel304"] = xu.materials.material.Amorphous(
+    "steel304", 7900, [("Fe", 0.68), ("Cr", 0.2), ("Ni", 0.1), ("Mn", 0.02)]
+)
+amorphous["PMMA"] = xu.materials.material.Amorphous(
+    "PMMA", 1190, [("C", 1), ("S", 1), ("H", 8), ("O", 2)]
+)
 
 # more useful values and constants
-#elementName = DummyClassDict(_consts.elementName)
-#meltPoint = DummyClassDict(_consts.meltPoint)
-#density = DummyClassDict(_consts.Density)
+# elementName = DummyClassDict(_consts.elementName)
+# meltPoint = DummyClassDict(_consts.meltPoint)
+# density = DummyClassDict(_consts.Density)
+
 
 class Gas(xu.materials.material.Amorphous):
-    def __init__(self,name, pressure=bar, temperature=295, molecule_size=1, atoms=None, cij=None):
+    def __init__(
+        self, name, pressure=bar, temperature=295, molecule_size=1, atoms=None, cij=None
+    ):
         """pressure in Pascal, temperature in Kelvin"""
         self.pressure = pressure
-        self.temperature  = temperature
+        self.temperature = temperature
         self.molecule_size = molecule_size
-        super(Gas,self).__init__(name,0,atoms=atoms,cij=cij)
-
+        super(Gas, self).__init__(name, 0, atoms=atoms, cij=cij)
 
     def _getdensity(self):
         """
@@ -95,56 +133,68 @@ class Gas(xu.materials.material.Amorphous):
         -------
         mass density in kg/m^3
         """
-        num_dens = self.pressure/k/self.temperature
-        return self._get_composition_mass()*num_dens*self.molecule_size
+        num_dens = self.pressure / k / self.temperature
+        return self._get_composition_mass() * num_dens * self.molecule_size
+
     density = property(_getdensity)
 
     def _get_composition_mass(self):
         w = 0
-        for atom,occ in self.base:
+        for atom, occ in self.base:
             w += atom.weight * occ
         return w
 
-gas['air'] = Gas('air',molecule_size=1.9917,atoms=[('N',1.562),('O',.42),('C',.0003),('Ar',.0094)])
-gas['He'] = Gas('He',molecule_size=1,atoms=[('He',1)])
-gas['N'] = Gas('N',molecule_size=2,atoms=[('N',1)])
 
+gas["air"] = Gas(
+    "air",
+    molecule_size=1.9917,
+    atoms=[("N", 1.562), ("O", 0.42), ("C", 0.0003), ("Ar", 0.0094)],
+)
+gas["He"] = Gas("He", molecule_size=1, atoms=[("He", 1)])
+gas["N"] = Gas("N", molecule_size=2, atoms=[("N", 1)])
 
 
 def get_chem_formula(material_base):
     """creates string representation from a chemical formula inside a Material class instance."""
-    string_rep = ''
-    for atom,amount in material_base:
+    string_rep = ""
+    for atom, amount in material_base:
         string_rep += atom.basename + str(amount)
     return string_rep
 
 
-def get_CS_Photo(ID,E=None):
-  """Returns the total photoabsorption cross section in m^2
-     ID is the element symbol
-     E is the photon energy (default is current LCLS value)
-     NOTE: This is per molecule if chemical formula given
-  """
-  ID=checkID(ID)
-  E = getE(E)/1000.
-  form=periodictable.formulas.parse_formula(ID)
-  CS=0
-  for id,atoms in form.atoms.items():
-    CS+=xraylib.CS_Photo(elementZ[id.symbol],E)*atoms*AtomicMass[id.symbol]/c['NA']/u['cm']**2
-  return CS
-
-def Dose(ID,FWHM,J=None,E=None):
-    """ Computes the absorbed dose in a solid in eV/atom (photoabsoption cross sections)
-        ID is chemical fomula : 'Si'
-        FWHM is the FEL spot size
-        J is the LCLS pulse energy in Joules (default is LCLS value)
-        E is photon energy in eV or keV (default is LCLS value)
-        If no density is specified will use default value
+def get_CS_Photo(ID, E=None):
+    """Returns the total photoabsorption cross section in m^2
+    ID is the element symbol
+    E is the photon energy (default is current LCLS value)
+    NOTE: This is per molecule if chemical formula given
     """
-    ID=checkID(ID)
-    E = getE(E)/1000.
-#    if J==None:
-#      J=pypsepics.get("SIOC:SYS0:ML00:AO627")/1000.
-    wo=FWHM/1.18
-    dose=2*J*CS_Photo(ID,E)*u['eV']/n.pi/wo**2/nAtoms(ID)
+    ID = checkID(ID)
+    E = getE(E) / 1000.0
+    form = periodictable.formulas.parse_formula(ID)
+    CS = 0
+    for id, atoms in form.atoms.items():
+        CS += (
+            xraylib.CS_Photo(elementZ[id.symbol], E)
+            * atoms
+            * AtomicMass[id.symbol]
+            / c["NA"]
+            / u["cm"] ** 2
+        )
+    return CS
+
+
+def Dose(ID, FWHM, J=None, E=None):
+    """Computes the absorbed dose in a solid in eV/atom (photoabsoption cross sections)
+    ID is chemical fomula : 'Si'
+    FWHM is the FEL spot size
+    J is the LCLS pulse energy in Joules (default is LCLS value)
+    E is photon energy in eV or keV (default is LCLS value)
+    If no density is specified will use default value
+    """
+    ID = checkID(ID)
+    E = getE(E) / 1000.0
+    #    if J==None:
+    #      J=pypsepics.get("SIOC:SYS0:ML00:AO627")/1000.
+    wo = FWHM / 1.18
+    dose = 2 * J * CS_Photo(ID, E) * u["eV"] / n.pi / wo**2 / nAtoms(ID)
     return dose
